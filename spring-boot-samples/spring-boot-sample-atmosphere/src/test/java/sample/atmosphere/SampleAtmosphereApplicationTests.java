d95a571d78bd8282a5906163432c6b0c8c4682ed
guyScheiner <guyscheiner@gmail.com>
2018-08-02 12:19:18 +0300

    Update SampleAtmosphereApplicationTests.java

--- spring-boot-samples/spring-boot-sample-atmosphere/src/test/java/sample/atmosphere/SampleAtmosphereApplicationTests.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/test/java/sample/atmosphere/SampleAtmosphereApplicationTests.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package sample.atmosphere;
-
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
